d1dc0d65d5a1623be50e06663fa60b12bd192f40
Pavel Karpovich <pavel.karpovich@outlook.com>
2024-02-21 00:04:49 +0100

    feat(main.go): add main functionality for telegram bot to fetch usernames by phone number

--- go.mod
+++ go.mod
@@ -1,3 +1,37 @@
-module tg-contacts-search
+module github.com/pkarpovich/tg-contacts-search
 
 go 1.22
+
+require (
+	github.com/gotd/td v0.97.0
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/nyaruka/phonenumbers v1.3.2
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/go-faster/errors v0.7.1 // indirect
+	github.com/go-faster/jx v1.1.0 // indirect
+	github.com/go-faster/xor v1.0.0 // indirect
+	github.com/gotd/ige v0.2.2 // indirect
+	github.com/gotd/neo v0.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.17.5 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	go.opentelemetry.io/otel v1.22.0 // indirect
+	go.opentelemetry.io/otel/trace v1.22.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,143 @@
-package tg_contacts_search
+package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/tg"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/nyaruka/phonenumbers"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"log"
+	"os"
+	"os/signal"
+)
+
+type Config struct {
+	BotToken string `env:"BOT_TOKEN"`
+	AppHash  string `env:"APP_HASH"`
+	AppId    int    `env:"APP_ID"`
+}
+
+var cfg Config
+
+func validatePhoneNum(phoneNum string) bool {
+	parsedNum, err := phonenumbers.Parse(phoneNum, "ZZ")
+	if err != nil {
+		return false
+	}
+
+	return phonenumbers.IsValidNumber(parsedNum)
+}
+
+func getUsername(client *tg.Client, phoneNum string) (string, error) {
+	users, err := client.ContactsImportContacts(context.Background(), []tg.InputPhoneContact{{Phone: phoneNum}})
+	if err != nil {
+		return "", fmt.Errorf("failed to import contacts: %w", err)
+	}
+
+	if len(users.GetUsers()) > 0 {
+		user, ok := users.GetUsers()[0].(*tg.User)
+		if !ok {
+			return "", nil
+		}
+
+		return user.Username, nil
+	}
+
+	return "", nil
+}
+
+func run(ctx context.Context, log *zap.Logger) error {
+	client := telegram.NewClient(cfg.AppId, cfg.AppHash, telegram.Options{})
+
+	return client.Run(ctx, func(ctx context.Context) error {
+		updates := tg.NewUpdateDispatcher()
+		opts := telegram.Options{
+			UpdateHandler: updates,
+			Logger:        log,
+		}
+
+		log.Info("Starting bot")
+
+		return telegram.BotFromEnvironment(ctx, opts, func(ctx context.Context, client *telegram.Client) error {
+			api := tg.NewClient(client)
+			sender := message.NewSender(tg.NewClient(client))
+
+			updates.OnNewMessage(func(ctx context.Context, entities tg.Entities, u *tg.UpdateNewMessage) error {
+				msg, ok := u.Message.(*tg.Message)
+				if !ok {
+					return nil
+				}
+				log.Info("Message", zap.Any("message", u.Message))
+
+				if msg.Message == "ping" {
+					_, err := sender.Reply(entities, u).Text(ctx, "pong")
+					if err != nil {
+						log.Error("failed to send message", zap.Error(err))
+					}
+
+					return nil
+				}
+
+				if !validatePhoneNum(msg.Message) {
+					_, err := sender.Reply(entities, u).Text(ctx, "Invalid phone number")
+					if err != nil {
+						log.Error("failed to send message", zap.Error(err))
+					}
+
+					return nil
+				}
+
+				username, err := getUsername(api, msg.Message)
+				if err != nil {
+					_, err := sender.Reply(entities, u).Text(ctx, fmt.Sprintf("Failed to get username: %s", err))
+					if err != nil {
+						log.Error("failed to send message", zap.Error(err))
+					}
+
+					return nil
+				}
+
+				if username == "" {
+					_, err := sender.Reply(entities, u).Text(ctx, "User not found")
+					if err != nil {
+						log.Error("failed to send message", zap.Error(err))
+					}
+
+					return nil
+				}
+
+				_, err = sender.Reply(entities, u).Text(ctx, fmt.Sprintf("https://t.me/%s", username))
+				if err != nil {
+					log.Error("failed to send message", zap.Error(err))
+					return nil
+				}
+
+				return nil
+			})
+
+			return nil
+		}, telegram.RunUntilCanceled)
+	})
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := cleanenv.ReadEnv(&cfg)
+	if err != nil {
+		log.Fatalf("Failed to read config.yml: %s", err)
+	}
+
+	logger, err := zap.NewDevelopment(zap.IncreaseLevel(zapcore.InfoLevel), zap.AddStacktrace(zapcore.FatalLevel))
+	if err != nil {
+		log.Fatalf("Failed to create logger: %s", err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	if err := run(ctx, logger); err != nil {
+		panic(err)
+	}
 }
